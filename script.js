6ab2f8ac7091db95358ce11899867b50e104330c
natalliakurbat <146668174+natalliakurbat@users.noreply.github.com>
2023-10-21 19:15:16 +0300

    добавила addEventListener

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
+const sendInput = document.querySelector('.input');
+const itemsList = document.querySelector('.items');
+
+if (sendInput) {
+    sendInput.addEventListener('keydown', function(event) {
+
+        if (event.key == 'Enter') {
+            const inputText = sendInput.value;
+
+            const newInput = document.createElement('div');
+            newInput.classList.add('items');
+            newInput.textContent = inputText;
+
+            if (itemsList) {
+                itemsList.append(newInput);
+            }
+        }
+    });
+}
 
 /* Пустые элементы не должны добавляться */
 
